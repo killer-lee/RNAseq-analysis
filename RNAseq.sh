b1ebd5f0d3bd61cdfcf15c6b15b5000efaac797a
康继昌(Kangjichang) <kangjichang@outlook.com>
2025-06-08 16:38:29 +0800

    Add files via upload

---
+++ RNAseq.sh
@@ -0,0 +1,2 @@
+cd /data1/kang/Learning/RNAseq_test/data1/SRR27922628
+python3 /data1/kang/Learning/RNAseq_test/RNA_seq.py -t 10 -r1 SRR27922628_1.fastq.gz -r2 SRR27922628_2.fastq.gz -r /data1/haol/kang/Learning/RNAseq_test/raw_data/musculus/grcm38/genome -a /data1/haol/kang/Learning/RNAseq_test/raw_data/musculus/annotation/Mus_musculus.GRCm38.102.gtf 
